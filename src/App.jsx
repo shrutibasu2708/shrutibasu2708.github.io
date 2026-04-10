28598808b69c6bf4080616b314e2470f543c8d50
Abhinav Kumar Roy <abhinav.roy23@gmail.com>
2026-04-10 16:27:42 -0700

    Start fresh: React + Vite + Tailwind v4 scaffold

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,7 @@
-import CustomCursor from "./components/CustomCursor";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Ticker from "./components/Ticker";
-import Expertise from "./components/Expertise";
-import Experience from "./components/Experience";
-import Deliverables from "./components/Deliverables";
-import Volunteer from "./components/Volunteer";
-import Tools from "./components/Tools";
-import Education from "./components/Education";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
-
 export default function App() {
   return (
-    <>
-      <CustomCursor />
-      <Navbar />
-      <main>
-        <Hero />
-        <Ticker />
-        <Expertise />
-        <Experience />
-        <Deliverables />
-        <Volunteer />
-        <Tools />
-        <Education />
-        <Contact />
-      </main>
-      <Footer />
-    </>
-  );
+    <div className="min-h-screen bg-white text-gray-900">
+      <h1 className="text-4xl font-bold p-8">Hello</h1>
+    </div>
+  )
 }
